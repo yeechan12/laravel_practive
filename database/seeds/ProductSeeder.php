b7c20dad5bb24f52f22d7e6610cbf598d32a042d
yeechan12 <hatran12hust@gmail.com>
2021-10-29 09:16:27 +0700

    add: seeder

---
+++ database/seeds/ProductSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Models\Product;
+use Illuminate\Database\Seeder;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Product::truncate();
+        factory(Product::class, 100)->create();
+    }
+}
